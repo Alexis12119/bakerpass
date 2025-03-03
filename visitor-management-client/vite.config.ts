433d3e2a8dc35f0f23aeafb104b9e8427bec3824
Alexis12119 <corporal461@gmail.com>
2025-03-04 07:09:51 +0800

    feat: update

--- visitor-management-client/vite.config.ts
+++ visitor-management-client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://trolley.proxy.rlwy.net:33436",
+        target: "http://trolley.proxy.rlwy.net:3001",
         changeOrigin: true,
       },
     },
